4a9557a32738b6286f2e7ee37a9b6cd2318a4a67
Ilya Maier <ilya.maier@revelsystems.com>
2020-05-20 19:30:36 +0300

    Project skeleton

---
+++ TopNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopNews
+//
+//  Created by Ilya Maier on 20.05.2020.
+//  Copyright © 2020 mera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
